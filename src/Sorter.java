976d34e0fb6e48dec60c5dca6f4e4a318ef1da9a
toastyblast <yorankerbusch@gmail.com>
2017-09-14 08:58:51 +0200

    Added back old files

---
+++ src/Sorter.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Sorter implements Runnable {
+    private List<Integer> myIntegerList;
+
+    public Sorter(List<Integer> integers) {
+        myIntegerList = integers;
+    }
+
+    @Override
+    public void run() {
+        //This run also does an insertion sort, a duplicate of the one in Main, as both main and multi threading need
+        // them separate.
+        int index = 0;
+
+        while (index < myIntegerList.size()) {
+            int currentNumber = myIntegerList.get(index);
+            int newIndex = index;
+
+            while (newIndex > 0 && currentNumber < myIntegerList.get(newIndex - 1)) {
+                myIntegerList.set(newIndex, myIntegerList.get(newIndex - 1));
+                myIntegerList.set((newIndex - 1), currentNumber);
+
+                newIndex--;
+            }
+
+            index++;
+        }
+    }
+
+    public List<Integer> getMyIntegerList() {
+        return myIntegerList;
+    }
+}
