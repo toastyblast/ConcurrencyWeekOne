da898d7e2b6124db39125668db6733b2f49f9d5e
Martin <mslavinovslavov@gmail.com>
2017-09-16 11:37:10 +0200

    Fixes

--- src/SortingPartThree.java
+++ src/SortingPartThree.java
@@ -16,54 +16,80 @@ public class SortingPartThree extends Thread {
 
     @Override
     public void run() {
-        if (list.size() >= max){
-            List<Integer> copyList = list;
-            List<Integer> list1 = new ArrayList<>();
-            List<Integer> list2 = new ArrayList<>();
 
-            for (int i = 0 ; i < max ; i++){
-                if (i < max/2)
-                    list1.add(copyList.get(i));
-                else
-                    list2.add(copyList.get(i));
-            }
+//        list.size() >= max
+//        list.size()%2 == 0
 
-            //TODO: Change the max here to influence how big the thresholds are!
-            Thread t1 = new SortingPartThree(list1, 5000);
-            Thread t2 = new SortingPartThree(list2, 5000);
+        if (list.size() % 2 == 0) {
+            if (list.size() >= max) {
 
-            t1.start();
-            t2.start();
+                List<Integer> copyList = list;
+                List<Integer> list1 = new ArrayList<>();
+                List<Integer> list2 = new ArrayList<>();
 
-            try {
-                t1.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                t2.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                for (int i = 0; i < list.size(); i++) {
+                    if (i < list.size() / 2)
+                        list1.add(list.get(i));
+                    else
+                        list2.add(list.get(i));
+                }
 
 
-            try {
-                mergeIntoExistingList(list1, list2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                Thread t1 = new SortingPartThree(list1, 100000);
+                Thread t2 = new SortingPartThree(list2, 100000);
+
+                t1.start();
+                t2.start();
+
+                try {
+                    t1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    t2.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    mergeIntoExistingList(list1, list2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
             for (int a = 1 ; a < list.size() ; a++){
                 if (list.get(a-1) > list.get(a)){
-                    System.out.println("FromThread");
+                    System.out.println("FromThread " + max);
                     System.out.println(list.get(a-1) + " " + list.get(a));
                     break;
                 }
             }
 
-            System.out.println("");
+                System.out.println("From branch" + list.size());
 
-        } else {
+            }
+            else {
+
+                int index = 1;
+                while (index < list.size()) {
+                    int element = list.get(index);
+                    int newIndex = index;
+                    while (newIndex > 0 && list.get(newIndex - 1) > element) {
+                        int newNumber = list.get(newIndex - 1);
+                        list.set(newIndex, newNumber);
+                        newIndex = newIndex - 1;
+                    }
+                    if (list.get(newIndex) >= element) {
+                        list.set(newIndex, element);
+                        index = index + 1;
+                    }
+                }
+                System.out.println(list.size() + "From leaf");
+            }
+        }
+        else {
 
             int index = 1;
             while (index < list.size()) {
@@ -79,7 +105,7 @@ public class SortingPartThree extends Thread {
                     index = index + 1;
                 }
             }
-
+            System.out.println(list.size() + "From leaf");
         }
     }
 
